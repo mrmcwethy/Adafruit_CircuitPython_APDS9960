9e22a80792e74a1a43819d67c67b3967a46e4b60
mrmcwethy <mrmcwethy@yahoo.com>
2018-01-02 13:36:21 -0700

    changed example to match README.rst

--- examples/proximity.py
+++ examples/proximity.py
@@ -4,7 +4,7 @@ import digitalio
 from adafruit_apds9960.apds9960 import APDS9960
 
 i2c = busio.I2C(board.SCL, board.SDA)
-int_pin = digitalio.DigitalInOut(board.A2)
+int_pin = digitalio.DigitalInOut(board.A1)
 apds = APDS9960(i2c, interrupt_pin=int_pin)
 
 apds.enable_proximity = True
